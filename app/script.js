c0d3cb72bee0637b3c9c9ed1c66257772d5eb6dd
Marta Majerowska <majeranek85@gmail.com>
2020-07-15 12:29:47 +0200

    Add state

--- app/script.js
+++ app/script.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { render } from 'react-dom';
 
 class App extends React.Component {
+  state = {
+    status: 'off',
+    time: null,
+    timer: null,
+  }
 
   render() {
     return (
